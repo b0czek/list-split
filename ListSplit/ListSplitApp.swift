dc277fad7faf0ac7ad80a5e6c09a7bb0a141c4a0
J M <272664@student.pwr.edu.pl>
2024-10-29 08:05:46 +0100

    Initial Commit

---
+++ ListSplit/ListSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListSplitApp.swift
+//  ListSplit
+//
+//  Created by stud on 29/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct ListSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
